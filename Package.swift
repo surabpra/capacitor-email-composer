a225521652aaa16dcc40f178812b250616e6d4c8
Surabhi Shinde <surabhi.prakashshinde.ext@bayer.com>
2025-10-23 14:21:11 +0530

    Restructure for Swift Package Manager compatibility

--- Package.swift
+++ Package.swift
@@ -1,32 +1,20 @@
 // swift-tools-version:5.9
 import PackageDescription
- 
+
 let package = Package(
     name: "CapacitorEmailComposer",
-    platforms: [
-        .iOS(.v13)
-    ],
+    platforms: [.iOS(.v13)],
     products: [
         .library(
             name: "CapacitorEmailComposer",
             targets: ["Plugin"]
         ),
     ],
-    dependencies: [
-        // Core Capacitor dependency
-        .package(url: "https://github.com/ionic-team/capacitor-swiftpm", from: "6.0.0"),
-    ],
     targets: [
         .target(
             name: "Plugin",
-            dependencies: [
-                .product(name: "Capacitor", package: "capacitor-swiftpm")
-            ],
-            path: "ios/Plugin",
-            publicHeadersPath: "include",
-            cSettings: [
-                .headerSearchPath("include")
-            ]
+            path: "Sources/Plugin",
+            publicHeadersPath: "."
         ),
     ]
 )
